cdaf085687c2999d1eaefa4d6c8e5069f6a85ce4
Diego Dominguez Alvarez <diegodoalbasket@gmail.com>
2016-12-24 15:13:01 +0100

    Created car structure

---
+++ Cars/Cars/SeatIbiza.swift
@@ -0,0 +1,48 @@
+//
+//  File.swift
+//  Cars
+//
+//  Created by Diego Dominguez Alvarez on 24/12/16.
+//  Copyright © 2016 Diego Dominguez Alvarez. All rights reserved.
+//
+
+import Foundation
+
+
+
+class SeatIbiza{
+    
+    var color = "Red"
+    var size = "Small"
+    var doors = 5
+    
+    func speeUp() {
+        
+    }
+    
+    func start(){
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Cars/Cars/carStore.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Cars
+//
+//  Created by Diego Dominguez Alvarez on 24/12/16.
+//  Copyright © 2016 Diego Dominguez Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class carStore: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        print("On viewDidLoad.....")
+    }
+
+
+    @IBAction func createCar(_ sender: Any) {
+        
+        print("On createCar.....")
+
+        
+    }
+    
+    @IBAction func showSpecs(_ sender: Any) {
+        print("On showSpecs.....")
+
+    }
+    
+    
+    @IBAction func startEngine(_ sender: Any) {
+        print("On startEngine.....")
+
+    }
+    
+    @IBAction func speedUp(_ sender: Any) {
+        print("On speedUp.....")
+
+    }
+    
+}
+
