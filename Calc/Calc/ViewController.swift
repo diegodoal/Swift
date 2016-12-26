5bcdb0bc15f06d738f4e6177a12ca05166401326
Diego Dominguez Alvarez <diegodoalbasket@gmail.com>
2016-12-26 14:06:08 +0100

    UI for calc application

---
+++ Calc/Calc/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Calc
+//
+//  Created by Diego Dominguez Alvarez on 26/12/16.
+//  Copyright © 2016 Diego Dominguez Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var labelRsult: UILabel!
+    
+    var result:Float = 0
+    var actualNum:Float = 0
+    var actualOp:String = "="
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    @IBAction func num(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func clearButtton(_ sender: UIButton) {
+    }
+    
+    
+    
+    @IBAction func op(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    
+}
+
